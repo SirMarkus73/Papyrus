87f40472f9429ea23517b359a51e503eb0592e1b
SirMarkus73 <marcosbarreroherrero@gmail.com>
2025-11-11 22:10:46 +0100

    chore(frontend): New styles to home page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,22 +6,20 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className='max-w-2/3 min-w-1/3 mx-auto min-h-screen grid place-items-center'>
-      <div className='bg-slate-800 p-3 grid gap-3 rounded-md'>
+      <div className='bg-neutral-950 my-3 mx-6 p-5 rounded-xl'>
           <header>
-            <h1 className='text-balance'>Manage your notes effortlessly</h1>
+            <h1 className='text-balance text-2xl font-bold font-sans'>Manage your notes effortlessly</h1>
           </header>
-          <main className='grid grid-cols-2 gap-3'>
-            <div className='bg-slate-700 rounded-md p-2 grid'>
+          <main className='grid grid-cols-2 gap-3 mt-4'>
+            <div className='bg-neutral-700 rounded-lg p-3 grid inset-shadow-xs inset-shadow-neutral-500 shadow-md shadow-neutral-800'>
               <h2 className='text-balance'>Do you have account?</h2>
-              <Link to='/login' className='bg-slate-700 border border-slate-600 p-2 rounded-lg mt-5 hover:bg-slate-800 transition-colors'>Log in</Link>
+              <Link to='/login' className='bg-neutral-700 border border-neutral-600 p-2 rounded-xl mt-5 hover:bg-neutral-800 transition-colors'>Log in</Link>
             </div>
-            <div className='bg-slate-700 rounded-md p-3 grid'>
+            <div className='bg-neutral-700 rounded-lg p-3 grid inset-shadow-xs inset-shadow-neutral-500 shadow-md shadow-neutral-800'>
               <h2 className='text-balance'>Don't have account?</h2>
-              <Link to="/register" className='bg-slate-700 border border-slate-600 p-2 rounded-lg mt-5 hover:bg-slate-800 transition-colors'>Register</Link>
+              <Link to="/register" className='bg-neutral-700 border border-neutral-600 p-2 rounded-xl mt-5 hover:bg-neutral-800 transition-colors'>Register</Link>
             </div>
           </main>
       </div>
-    </div>
   )
 }
